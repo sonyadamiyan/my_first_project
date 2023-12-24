1d72166292fcc0e7c979feb8e6c04416b97ee1e5
sonyadamiyan <sonyadamiyan7@gmail.com>
2023-12-24 11:20:02 +0300

    telegram

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("Hello, world")
-print(5)
+from telebot import TeleBot
+
+bot = TeleBot('5850797962:AAFukAKCIe7Cw2EcG2pczUkK_ZPr2DoRFRo')
+
+def filter_password(message):
+    password = "хомяк"
+    return password in message.text
+
+@bot.message_handler(content_types=['text'], func = filter_password)
+def say_hello(message):
+    bot.send_message(message.chat.id, "Привет!")
+
+bot.polling()
